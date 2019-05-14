6221f8cb2fab004f69a614a9073cb24af7ce0c44
Vladyslav Butenko <vbutenko713@gmail.com>
2019-05-14 20:02:44 +0300

    Added UserRepo(Unfinished). AppInitializer config has been completed.

--- src/main/java/com/butenko/vladyslav/foxconstructions/config/AppInitializer.java
+++ src/main/java/com/butenko/vladyslav/foxconstructions/config/AppInitializer.java
@@ -1,13 +1,21 @@
 package com.butenko.vladyslav.foxconstructions.config;
 
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterRegistration;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
 
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class<?>[] {WebConfig.class};
+        return new Class<?>[]{WebConfig.class};
     }
 
     @Override
@@ -21,6 +29,26 @@ public class AppInitializer extends AbstractAnnotationConfigDispatcherServletIni
 
     @Override
     protected String[] getServletMappings() {
-        return new String[] {"/"};
+        return new String[]{"/"};
+    }
+
+    @Override
+    //Правильное отображение символов
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        super.onStartup(servletContext);
+        Filter filter = new CharacterEncodingFilter();
+        FilterRegistration.Dynamic dynamic = servletContext.addFilter("encodingFilter", filter);
+        dynamic.setInitParameter("encoding", "UTF-8");
+        dynamic.setInitParameter("forceEncoding", "true");
+        dynamic.addMappingForUrlPatterns(null, true, "/*");
+    }
+
+    @Override
+    //Чтобы не выбивало 404, будет вместо него кидать Exc
+    protected DispatcherServlet createDispatcherServlet(WebApplicationContext servletAppContext) {
+        DispatcherServlet dispatcherServlet
+                = (DispatcherServlet) super.createDispatcherServlet(servletAppContext);
+        dispatcherServlet.setThrowExceptionIfNoHandlerFound(true);
+        return dispatcherServlet;
     }
 }
---
+++ src/main/java/com/butenko/vladyslav/foxconstructions/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.butenko.vladyslav.foxconstructions.repository;
+
+import com.butenko.vladyslav.foxconstructions.model.user.User;
+
+public interface UserRepository extends MainRepository<User> {
+
+}
